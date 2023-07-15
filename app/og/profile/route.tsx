e731c9351b99e8c9a537c3ba0c25eae3c7f8d006
Jasper Concepcion <jasper@manok.dev>
2023-07-15 17:47:53 +0800

    fix(og): websiteLink is not parsed properly

--- app/og/profile/route.tsx
+++ app/og/profile/route.tsx
@@ -7,7 +7,7 @@ import { LinkIcon } from "./_link_icon"
 export const runtime = "edge"
 
 const interBold = fetch(new URL("../../../assets/Inter-Bold.ttf", import.meta.url)).then((res) =>
-  res.arrayBuffer()
+  res.arrayBuffer(),
 )
 
 export async function GET(req: NextRequest) {
@@ -111,7 +111,7 @@ export async function GET(req: NextRequest) {
             <div style={{ display: "flex", alignItems: "center", gap: 10 }}>
               <LinkIcon />
               <div style={{ fontSize: 20, color: "rgba(255,255,255,0.5)" }}>
-                {website.replace(new RegExp(/.*\/\//), "").replace(/\/$/g, "")}
+                {website.replace(new RegExp(/https:\/\//), "").replace(/\/$/g, "")}
               </div>
             </div>
           )}
@@ -128,6 +128,6 @@ export async function GET(req: NextRequest) {
           data: await interBold,
         },
       ],
-    }
+    },
   )
 }
