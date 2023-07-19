becae701678c09eb28d460a44ded8b14f976fa7e
Jasper Concepcion <jasper@manok.dev>
2023-07-20 05:56:03 +0800

    fix: use process.env for siteconfig

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,6 @@
-import { env } from "@/env.mjs"
-
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: env.NEXT_PUBLIC_NAME,
+  name: process.env.NEXT_PUBLIC_NAME || "",
   description: "A full-stack portfolio",
 }
